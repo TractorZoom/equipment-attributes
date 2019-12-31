e52715110251291fc9cadfaac0ed0e88cc66a053
twebber90 <twebber@tractorzoom.com>
2019-12-31 09:59:51 -0600

    docs: add comments to further describe attributes

--- src/attributes.js
+++ src/attributes.js
@@ -1,43 +1,222 @@
+/* 
+    Input of number of acres worked
+*/
 export const ACRES = 'acres'
+
+/*
+    NEEDS INFO
+*/
 export const BELLY_MOWER = 'bellyMower'
+
+/*
+    NEEDS INFO
+*/
 export const CAB = 'cab'
+
+/*
+    Input of capacity of equipment
+*/
 export const CAPACITY = 'capacity'
+
+/*
+    Select of equipment categories
+    Options: Ag Trailers, Chemical Applicators, Harvesting, Hay and Forage, Manure Handling, Other Equipment, Planting, Tillage, Tractors
+*/
 export const CATEGORY = 'category'
+
+/*
+    NEEDS INFO
+*/
 export const CENTER_BULK_FILL = 'centerBulkFill'
+
+/*
+    Input of city
+*/
 export const CITY = 'city'
+
+/*
+    NEEDS INFO
+*/
 export const CHOPPER = 'chopper'
+
+/*
+    Select of equipment condition
+    Options: New, Used
+*/
 export const CONDITION = 'condition'
+
+/*
+    Input of auctioneer description
+*/
 export const DESCRIPTION = 'description'
+
+/*
+    Select of equipment drive
+    Options: 
+*/
 export const DRIVE = 'drive'
+
+/*
+    NEEDS INFO
+*/
 export const EMISSIONS = 'emissions'
+
+/*
+    NEEDS INFO
+*/
 export const FRONT_DUALS = 'frontDuals'
+
+/*
+    NEEDS INFO
+*/
 export const FRONT_SINGLES = 'frontSingles'
+
+/*
+    Input of equipment hours
+*/
 export const HOURS = 'hours'
+
+/*
+    Input of equipment horsepower
+*/
 export const HORSEPOWER = 'horsepower'
+
+/*
+    Input of equipment length
+*/
 export const LENGTH = 'length'
+
+/*
+    NEEDS INFO
+*/
 export const LOADER = 'loader'
+
+/*
+    Input of lot number for equipment in an auction
+*/
 export const LOT_NUMBER = 'lotNumber'
+
+/*
+    Select of makes for equipment
+    Options: Dynamic list from database
+*/
 export const MAKE = 'make'
+
+/*
+    Input of miles driven for equipment
+*/
 export const MILES = 'miles'
+
+/*
+    Select of models based on make for equipment
+    Options: Dynamic list from database based on make
+*/
 export const MODEL = 'model'
+
+/*
+    Input of number of bales for equipment
+*/
 export const NUMBER_OF_BALES = 'numberOfBales'
+
+/*
+    Input of number of bottoms for equipment
+*/
 export const NUMBER_OF_BOTTOMS = 'numberOfBottoms'
+
+/*
+    Input of number of rows for equipment
+*/
 export const NUMBER_OF_ROWS = 'numberOfRows'
+
+/*
+    Input of number of shanks for equipment
+*/
 export const NUMBER_OF_SHANKS = 'numberOfShanks'
+
+/*
+    Input of price for equipment 
+*/
 export const PRICE = 'price'
+
+/*
+    NEEDS INFO
+*/
 export const POWERFOLD_BIN = 'powerfoldBin'
-export const REAR_DUALS = 'reatDuals'
+
+/*
+    NEEDS INFO
+*/
+export const REAR_DUALS = 'rearDuals'
+
+/*
+    Select of rear PTO
+    Options: Yes, No
+*/
 export const REAR_PTO = 'rearPTO'
+
+/*
+    NEEDS INFO
+*/
 export const ROW_CLEANERS = 'rowCleaners'
+
+/*
+    Input of row spacing for equipment
+*/
 export const ROW_SPACING = 'rowSpacing'
+
+/*
+    Input of separator hours for equipment
+*/
 export const SEPARATOR_HOURS = 'separatorHours'
+
+/*
+    Input of serial number for equipment
+*/
 export const SERIAL = 'serial'
+
+/*
+    NEEDS INFO
+*/
 export const SPREADER = 'spreader'
+
+/*
+    Input of state
+*/
 export const STATE = 'state'
+
+/*
+    Select of subcategories based on category for equipment
+    Options: Dynamic list from database based on category
+*/
 export const SUBCATEGORY = 'subcategory'
+
+/*
+    NEEDS INFO
+*/
 export const TAG_AXLE = 'tagAxle'
+
+/*
+    NEEDS INFO
+*/
 export const THREE_POINT_HITCH = 'threePointHitch'
+
+/*
+    Select of transmission for equipment
+    Options: Automatic, Manual
+*/
 export const TRANSMISSION = 'transmission'
+
+/*
+    Input of width for equipment
+*/
 export const WIDTH = 'width'
+
+/*
+    Input of year for equipment
+*/
 export const YEAR = 'year'
+
+/*
+    Input of zip code
+*/
 export const ZIP_CODE = 'zipCode'
