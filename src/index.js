c4cede9ad184874dc341c39232b7b4bc25e3f094
twebber90 <twebber@tractorzoom.com>
2021-04-19 08:41:39 -0500

    feat: add utilites to get variable detail and location

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const categories = require('./categories');
 const attributesByCategory = require('./attributes-by-category');
+const categories = require('./categories');
+const format = require('./format');
+const location = require('./location');
 const units = require('./units');
+const variableDetail = require('./variable-detail');
 
 module.exports = {
     COMMON_ATTRIBUTES: attributesByCategory.COMMON_ATTRIBUTES,
@@ -9,7 +12,10 @@ module.exports = {
     LOCATION_ATTRIBUTES: attributesByCategory.LOCATION_ATTRIBUTES,
     UNITS: units,
     VALUE_ATTRIBUTES: attributesByCategory.VALUE_ATTRIBUTES,
+    formatNumberWithThousandSeparator: format.formatNumberWithThousandSeparator,
     getAttributesForCategory: attributesByCategory.getAttributesForCategory,
     getClearableAttributesForCategory: attributesByCategory.getClearableAttributesForCategory,
+    getLocation: location.getLocation,
     getSubcategoriesForCategory: categories.getSubcategoriesForCategory,
+    getVariableDetail: variableDetail.getVariableDetail,
 };
